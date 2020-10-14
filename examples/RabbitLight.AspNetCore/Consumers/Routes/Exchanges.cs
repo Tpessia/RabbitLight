052db9450c35c186af25d95e9d42d64cc834ca98
Pessia, Thiago <thiago.pessia@gmail.com>
2020-10-14 17:37:38 -0300

    Folder structure refactor

--- examples/RabbitLight.AspNetCore/Consumers/Routes/Exchanges.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace RabbitLight.AspNetCore.Consumers.Routes
-{
-    public class Exchanges
-    {
-        public const string Test1 = "test1";
-        public const string Test2 = "test2";
-    }
-}
